b0a56a43f443c928183662862c4f4990750b49dd
Saurin Modi <saurinmodi.imp@gmail.com>
2026-09-20 20:22:02 +0530

    added routing capability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import {BrowserRouter as Router,Routes,Route} from "react-router"
 import "./App.css";
 import Home from "./components/Home";
 
 function App() {
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+      <Home />
+      }/>
+      </Routes>
+    </Router>
       <Home />
     </>
   );
