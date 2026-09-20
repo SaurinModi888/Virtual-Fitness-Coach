5b6273f35a56122bb7e0856c5cd312c031f02f04
DaivikModh <daivikmodh9@gmail.com>
2026-09-20 19:36:47 +0530

    initial commit

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,20 @@
-import { axios } from "axios";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
-  const data = axios.get("http://127.0.0.1:5000/api/test");
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/api/test")
+      .then((response) => {
+        setData(response.data.message);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <>
       <h1>From Home element</h1>
@@ -10,4 +23,4 @@ function Home() {
   );
 }
 
-export default Home();
+export default Home;
